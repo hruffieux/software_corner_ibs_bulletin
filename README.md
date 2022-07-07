1f6534a54b073fe8b903cd3b28a0514a66e79988
HélènRuffieux <helene.ruffieux@mrc-bsu.cam.ac.uk>
2022-07-08 00:30:16 +0200

    Adapted tutorial for a single analysis.

--- README.md
+++ README.md
@@ -1,36 +1,17 @@
-## Software corner IBS bulletin: atlasqtl – an R package for variable selection in sparse regression with hierarchically-related responses
+## Software corner IBS bulletin on atlasqtl
 
 ### Data
 
-The genotyping data are protected. We are therefore using a synthetic dataset emulating the real data. 
-The expression and replicated genotyping data are in 
-[data/replicated_data.RData](https://github.com/hruffieux/software_corner_ibs_bulletin/blob/master/data/replicated_data.RData). The ready-to-use data with simulated genetic associations are in 
+The genotyping data are protected. We are therefore using a synthetic dataset 
+emulating the real data. The expression and replicated genotyping data are in 
+[data/replicated_data.RData](https://github.com/hruffieux/software_corner_ibs_bulletin/blob/master/data/replicated_data.RData). The ready-to-use data with simulated genetic associations 
+are in 
 [data/prepared_data.RData](https://github.com/hruffieux/software_corner_ibs_bulletin/blob/master/data/prepared_data.RData).
 
-**Important note:** these are large files which are stored using Git Large File Storage. To clone these
-files along with the repository, please install Git LFS, e.g., using Homebrew:
-
-``` bash
-brew install git-lfs
-```
-
-and, within the cloned repository, initialise it for your account by using:
-
-``` bash
-git lfs install
-```
-
-and retrieve all large files:
-
-``` bash
-git lfs pull
-```
-
-(Alternatively, since there are only two such files, they can be downloaded manually from the Github interface.)
-
 The file [data/prepared_data.RData](https://github.com/hruffieux/software_corner_ibs_bulletin/blob/master/data/prepared_data.RData) 
-is obtained by updating the real expression data to simulate genetic associations between the synthetic 
-genotyping data and the transcript levels. This last step is obtained by running the R file 
+is obtained by updating the real expression data to simulate genetic 
+associations between the synthetic genotyping data and the transcript levels. 
+This last step is obtained by running the R file 
 [scripts/prepare_data.R](https://github.com/hruffieux/software_corner_ibs_bulletin/blob/master/scripts/prepare_data.R) 
 after installing the R package **echoseq**:
 
@@ -52,7 +33,7 @@ remotes::install_github("hruffieux/atlasqtl")
 
 The eQTL analysis can be run using the Rmarkdown script: 
 [scripts/atlasqtl_software_corner.Rmd](https://github.com/hruffieux/software_corner_ibs_bulletin/blob/master/scripts/atlasqtl_software_corner.Rmd). 
-This file also provides step-by-step guidance for the use and settings of the **atlasqtl** for our example.
+This file also acts as a simple tutorial for the use of **atlasqtl**.
 
 ### Issues
 
--- scripts/prepare_data.R
+++ scripts/prepare_data.R
@@ -25,22 +25,19 @@ bool_save <- TRUE
 ## -- It can't be regenerated as we do not provide the real ----
 ## -- genotyping data but the expression data and synthetic ----
 ## -- genotyping data obtained by emulating the real can be ----
-## -- downloaded from data/replicated_data.R and loaded as 
+## -- downloaded from data/replicated_data.RData and loaded as 
 ## -- done below -
 #
 load(file.path(data_dir, "replicated_data.RData"))
 
-vec_type <- names(list_expr)
+p <- ncol(X) # SNPs
+q <- ncol(Y_original) # B cell expression data
 
-p <- unique(sapply(list_repl_snps, ncol))
-stopifnot(length(p) == 1) # same number of of SNPs for all conditions
-
-q <- unique(sapply(list_expr, ncol))
-stopifnot(length(q) == 1) # same number of of levels for all conditions
+stopifnot(nrow(X) == nrow(Y_original)) # same number of samples for SNPs and expression levels
+n <- nrow(X)
+rownames(Y_original) <- rownames(X)
 
 ## simulates association pattern between the SNPs and the expression levels
-# same active SNPs and active levels for all conditions
-# but a given active SNP may be associated with different active levels across the conditions
 #
 p0 <- 5  # number of active SNPs
 ind_p0 <- sort(sample(1:p, p0, replace = FALSE))
@@ -54,44 +51,24 @@ vec_prob_sh <- rbeta(p0, shape1 = 1, shape2 = 2)
 
 max_tot_pve <- 0.5 # maximum total proportion of outcome variance explained by the SNPs.
 
-list_data <- NULL
-for (type in vec_type) {
+obj_snps <- convert_snps(X)
+obj_expr <- convert_phenos(Y_original)
 
-  replicated_snps <- list_repl_snps[[type]]
-  original_expr <- list_expr[[type]]
+# add some noise so these propensities differ slightly depending on the condition
+vec_prob_sh_type <- vec_prob_sh + rnorm(p0, sd = 0.1)
 
-  n <- unique(c(nrow(replicated_snps), nrow(original_expr)))
-  stopifnot(length(n) == 1) # same number of samples for SNPs and expression levels
-  
-  rownames(original_expr) <- rownames(replicated_snps)
+# make sure that between zero and one
+vec_prob_sh_type[vec_prob_sh_type < .Machine$double.eps | vec_prob_sh_type > 1 - .Machine$double.eps] <- vec_prob_sh[vec_prob_sh_type < .Machine$double.eps | vec_prob_sh_type > 1 - .Machine$double.eps]
 
-  obj_snps <- convert_snps(replicated_snps)
-  obj_expr <- convert_phenos(original_expr)
+obj_data <- generate_dependence(obj_snps, obj_expr, ind_q0, ind_p0,
+                                vec_prob_sh_type, family = "gaussian",
+                                max_tot_pve = max_tot_pve)
 
-  # add some noise so these propensities differ slightly depending on the condition
-  vec_prob_sh_type <- vec_prob_sh + rnorm(p0, sd = 0.1)
-  
-  # make sure that between zero and one
-  vec_prob_sh_type[vec_prob_sh_type < .Machine$double.eps | vec_prob_sh_type > 1 - .Machine$double.eps] <- vec_prob_sh[vec_prob_sh_type < .Machine$double.eps | vec_prob_sh_type > 1 - .Machine$double.eps]
-  
-  obj_data <- generate_dependence(obj_snps, obj_expr, ind_q0, ind_p0,
-                                  vec_prob_sh_type, family = "gaussian",
-                                  max_tot_pve = max_tot_pve)
+stopifnot(all.equal(rownames(obj_data$snps), rownames(obj_data$phenos)))
 
-  stopifnot(all.equal(rownames(obj_data$snps), rownames(obj_data$phenos)))
-
-  list_data <- append(list_data,
-                      list(list("snps" = obj_data$snps, "expr" = obj_data$phenos)))
-}
-names(list_data) <- vec_type
-
-# Check that same set of SNPs and expression levels across conditions.
-list_snp_names <- lapply(list_data, function(ll_type) names(ll_type$snps))
-stopifnot(length(unique(list_snp_names)) == 1)
-
-list_expr_names <- sapply(list_data, function(ll_type) names(ll_type$expr))
-stopifnot(length(unique(list_expr_names)) == 1)
+X <- obj_data$snps
+Y <- obj_data$phenos
 
 if (bool_save) {
-  save(list_data, file = file.path(data_dir, "prepared_data.RData"))
+  save(X, Y, file = file.path(data_dir, "prepared_data.RData"))
 }
